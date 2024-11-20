a032844adb8d43fd35a6248239da9909c628803e
antarnath <antar.ch.nath@gmail.com>
2024-11-21 01:23:30 +0600

    search complete

--- doctor/urls.py
+++ doctor/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
   path('home/', Home.as_view(), name='home'),
+  path('search/', SearchView.as_view(), name='search'), 
 ] 
--- doctor/views.py
+++ doctor/views.py
@@ -8,9 +8,9 @@ from rest_framework import status
 
 class Home(APIView):
   def get(self, request, *args, **kwargs):
-    hospitals = Hospital.objects.all()
-    specialities = Specialities.objects.all()
-    doctors = Doctor.objects.all()
+    hospitals = Hospital.objects.all()[:10]
+    specialities = Specialities.objects.all()[:10]
+    doctors = Doctor.objects.all()[:10]
 
     response_data = {
       "hospitals": HospitalSerializer(hospitals, many=True).data,
@@ -19,3 +19,31 @@ class Home(APIView):
     }
 
     return Response(response_data, status=status.HTTP_200_OK) 
+  
+  
+
+class SearchView(APIView):
+  def get(self, request, *args, **kwargs):
+    search_query = request.query_params.get('search', None)  # Get the search input
+
+    if not search_query:
+      return Response({"error": "Search query is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    # Search Hospitals
+    hospitals = Hospital.objects.filter(name__icontains=search_query)
+
+    # Search Doctors
+    doctors = Doctor.objects.filter(name__icontains=search_query)
+
+    # Search Specialities and retrieve related doctors
+    specialities = Specialities.objects.filter(name__icontains=search_query)
+    doctors_by_speciality = Doctor.objects.filter(speciality__in=specialities)
+
+    # Serialize the results
+    response_data = {
+      "hospitals": HospitalSerializer(hospitals, many=True).data,
+      "doctors": DoctorSerializer(doctors, many=True).data,
+      "doctors_by_speciality": DoctorSerializer(doctors_by_speciality, many=True).data, 
+    }
+
+    return Response(response_data, status=status.HTTP_200_OK)
